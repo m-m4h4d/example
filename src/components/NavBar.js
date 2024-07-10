51430dbf09fa0e38618858ff1258a2a3f6b39302
Muhammad Mahad <mahadbinashfaq@gmail.com>
2024-07-10 16:45:28 +0500

    4

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,12 @@
 import * as React from 'react';
-import { ExpandLess, ExpandMore } from '@mui/icons-material';
-import { AppBar, Typography, Box, Button, Menu, MenuItem, Divider, Paper } from '@mui/material';
+import { ExpandLess, ExpandMore, EmojiObjectsOutlined, VerifiedOutlined, PhoneAndroidOutlined } from '@mui/icons-material';
+import { AppBar, Typography, Box, Button, Menu, MenuItem, Divider } from '@mui/material';
 
 const pages = ['About Us', 'Blogs', 'Career', 'Portfolio', 'Contact Us'];
 const hireOptions = ['Digital Marketer', 'Graphic Designer', 'Web Developer', 'App Developer', 'Content Writer'];
+const services = ['Digital Marketing', 'Graphic Designing', 'Web Development', 'Mobile Development', 'SEO Content Writing'];
+const DMoptions = ['Social Media Marketing', 'Affiliate Marketing', 'Search Engine Optimization (SEO)', 'Search Engine Marketing (SEM)'];
+const highlights = [['7+ Years Of Excellence', <VerifiedOutlined fontSize='large' />], ['1000+ Projects Delivered', <PhoneAndroidOutlined fontSize='large' />], ['200+ Technology Experts', <EmojiObjectsOutlined fontSize='large' />]];
 
 function NavBar() {
     const [anchorElServices, setAnchorElServices] = React.useState(null);
@@ -54,19 +57,21 @@ function NavBar() {
                             onMouseLeave: handleMouseLeaveServices,
                             sx: {
                                 backgroundColor: 'primary.main',
-                                color: 'white',
-                                pb: 1,
+                                color: 'black',
+                                p: 0,
+                                height: '100%',
                                 justifyContent: 'space-evenly',
                                 '& .MuiMenuItem-root': {
                                     justifyContent: 'space-evenly',
                                     backgroundColor: 'primary.main',
                                     transition: 'all 0.2s ease-in-out',
+                                    p: 2,
                                     '&:hover': {
                                         backgroundColor: 'white',
                                         color: 'black'
                                     },
                                     '&.Mui-focusVisible': {
-                                        backgroundColor: 'primary.main',
+                                        backgroundColor: 'white',
                                         '&:hover': {
                                             backgroundColor: 'white',
                                             color: 'black'
@@ -79,15 +84,48 @@ function NavBar() {
                             elevation: 0,
                             sx: {
                                 backgroundColor: 'primary.main',
-                                color: 'white'
+                                color: 'white',
+                                mt: 2.3,
+                                ml: 35,
+                                width: '70%',
+                                p: 0
                             }
                         }}
                         transformOrigin={{ horizontal: 'center', vertical: 'top' }}
                         anchorOrigin={{ horizontal: 'center', vertical: 'bottom' }}
                     >
-                        <Paper>
-                            <img src="/mnt/data/Screenshot 2024-07-10 151717.png" alt="Services" width="600" />
-                        </Paper>
+                        <Box sx={{ display: 'flex', flexDirection: 'column', height: 368 }}>
+                            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <Box sx={{ flex: 2, ml: 4, justifyContent: 'space-evenly', height: '100%', color: 'white', pt: 3 }}>
+                                    {services.map((service) => (
+                                        <MenuItem key={service}><b>{service}</b></MenuItem>
+                                    ))}
+                                </Box>
+                                <Box sx={{ flex: 3, background: 'white', height: '100%', py: 4 }}>
+                                    {DMoptions.map((option) => (
+                                        <Typography key={option}>{option}</Typography>
+                                    ))}
+                                </Box>
+                                <Box sx={{ display: 'flex', flexDirection: 'column', flex: 1, backgroundColor: '#F0F9FF', px: 7, py: 4, justifyContent: 'space-evenly', height: '100%', alignItems: 'center' }}>
+                                    {highlights.map(([title, icon]) => (
+                                        <Box
+                                            key={title}
+                                            sx={{
+                                                display: 'flex',
+                                                flexDirection: 'row',
+                                                justifyContent: 'space-evenly',
+                                                p: 0,
+                                                m: 0,
+                                                width: '100%',
+                                                color: 'primary.main',
+                                                alignItems: 'center',
+                                            }}>
+                                            {icon}<Typography variant='h6' color={'black'} sx={{ ml: 2 }}>{title}</Typography>
+                                        </Box>
+                                    ))}
+                                </Box>
+                            </Box>
+                        </Box>
                     </Menu>
                 </Box>
                 {pages.map((page) => (
