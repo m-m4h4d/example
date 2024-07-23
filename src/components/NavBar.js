24f6f9d73d99327044dd5839815568080e34727e
Muhammad Mahad <mahadbinashfaq@gmail.com>
2024-07-23 16:15:06 +0500

    Updated NavBar Styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import MobileNavbar from './MobileNavbar';
-import { ExpandLess, ExpandMore, EmojiObjectsOutlined, VerifiedOutlined, PhoneAndroidOutlined } from '@mui/icons-material';
-import { AppBar, Typography, Box, Button, Menu, MenuItem, Divider, Container, Toolbar, Grid, ListItemText } from '@mui/material';
+import { ExpandLess, ExpandMore, EmojiObjectsOutlined, VerifiedOutlined, PhoneAndroidOutlined, Menu as MenuIcon, Close } from '@mui/icons-material';
+import { AppBar, Typography, Box, Button, Menu, MenuItem, Divider, Container, Toolbar, Grid, ListItemText, IconButton, Paper, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
 import {
     SocialMediaMarketing,
     AffiliateMarketing,
@@ -68,12 +67,51 @@ const highlights = [
     { title: '200+ Technology Experts', icon: <EmojiObjectsOutlined fontSize='large' /> }
 ];
 
+const serviceOptions = {
+    'Digital Marketing': [
+        'Social Media Marketing',
+        'Affiliate Marketing',
+        'Search Engine Optimization (SEO)',
+        'Search Engine Marketing (SEM)'
+    ],
+
+    'Graphic Designing': [
+        'Logo And Brand Identity',
+        'Web And App Design',
+        'Visual Design',
+        'Marketing Design'
+    ],
+
+    'Web Development': [
+        'Business Website Design',
+        'ECommerce Web Design',
+        'Landing Page Design',
+        'Dropshipping Website',
+        'Custom Web Development',
+        'Platform Design'
+    ],
+
+    'Mobile Development': [
+        'Android App Development',
+        'IOS App Development'
+    ],
+
+    'SEO Content Writing': [
+        'Technical Writing Services',
+        'Research and Analysis',
+        'Elearning Content'
+    ],
+}
+
 function NavBar() {
     const [anchorElHire, setAnchorElHire] = React.useState(null);
     const openHire = Boolean(anchorElHire);
     const [anchorElServices, setAnchorElServices] = React.useState(null);
     const openServices = Boolean(anchorElServices);
     const [currentService, setCurrentService] = React.useState(null);
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [expanded, setExpanded] = React.useState(false);
+    const servicesRef = React.useRef(null);
 
     const handleMouseEnterServices = (event) => {
         setAnchorElServices(event.currentTarget);
@@ -100,6 +138,36 @@ function NavBar() {
         setAnchorElHire(null);
     };
 
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+        setExpanded(false);
+    };
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+
+    React.useEffect(() => {
+        if (expanded === 'panel1' && servicesRef.current) {
+            window.scrollTo({
+                top: servicesRef.current.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    }, [expanded]);
+
+    const getHoverStyles = (panel) => ({
+        '&:hover': {
+            backgroundColor: expanded !== panel ? 'primary.dark' : 'inherit',
+            color: expanded !== panel ? 'white' : 'inherit',
+            cursor: expanded !== panel ? 'pointer' : 'default',
+        },
+    });
+
     const getOptions = (service) => {
         switch (service) {
             case 'Digital Marketing':
@@ -121,7 +189,117 @@ function NavBar() {
         <AppBar sx={{ background: 'white', color: 'black', justifyContent: 'center', alignItems: 'center', textAlign: 'center', position: '' }}>
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
-                    <MobileNavbar />
+                    <Box sx={{ display: { xs: 'flex', sm: 'flex', md: 'flex', lg: 'none', xl: 'none' }, justifyContent: 'flex-start', p: 0, m: 0, width: '10%' }}>
+                        <Typography variant='h3' sx={{ flexGrow: 1, mx: 1 }}>LOGO</Typography>
+                    </Box>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'flex', md: 'flex', lg: 'none', xl: 'none' }, justifyContent: 'flex-end', width: '100%' }}>
+                        <IconButton
+                            size="large"
+                            aria-label="open drawer"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={anchorElNav ? handleCloseNavMenu : handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            {anchorElNav ? <Close /> : <MenuIcon />}
+                        </IconButton>
+                        {anchorElNav && (
+                            <Paper
+                                id="menu-appbar"
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'center',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    position: 'fixed',
+                                    top: 60,
+                                    left: 0,
+                                    right: 0,
+                                    bottom: 0,
+                                    zIndex: 10,
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    color: 'primary.main',
+                                    overflowY: 'auto'
+                                }}
+                            >
+                                {pages.map((page) => (
+                                    <><MenuItem
+                                        key={page}
+                                        onClick={handleCloseNavMenu}
+                                        sx={{
+                                            color: 'primary.main',
+                                            '&:hover': {
+                                                backgroundColor: 'primary.dark',
+                                                color: 'white',
+                                                cursor: 'pointer',
+                                            },
+                                        }}>
+                                        <Typography textAlign="center">{page}</Typography>
+                                    </MenuItem>
+                                        <Divider /></>
+                                ))}
+                                <Accordion
+                                    expanded={expanded === 'panel1'}
+                                    onChange={handleChange('panel1')}
+                                    elevation={0}
+                                    sx={{
+                                        m: 0,
+                                        p: 0,
+                                        color: 'primary.main',
+                                        ...getHoverStyles('panel1'),
+                                    }}>
+                                    <AccordionSummary expandIcon={<ExpandMore />} aria-controls="panel1d-content" id="panel1d-header" sx={{ my: 0, py: 0 }}>
+                                        <Typography textAlign="center">Services</Typography>
+                                    </AccordionSummary>
+                                    <AccordionDetails ref={servicesRef} sx={{ my: 0, py: 0 }}>
+                                        {Object.keys(serviceOptions).map((service) => (
+                                            <React.Fragment key={service}>
+                                                <MenuItem onClick={handleChange('panel1')} sx={{ color: 'black' }}>
+                                                    <Typography>{service}</Typography>
+                                                </MenuItem>
+                                                <Box sx={{ display: 'flex', flexDirection: 'column', pl: 2 }}>
+                                                    {serviceOptions[service].map((subOption) => (
+                                                        <MenuItem key={subOption} onClick={handleChange('panel1')}>
+                                                            <Typography sx={{ color: 'primary.main' }}>{subOption}</Typography>
+                                                        </MenuItem>
+                                                    ))}
+                                                </Box>
+                                            </React.Fragment>
+                                        ))}
+                                    </AccordionDetails>
+                                </Accordion>
+                                <Accordion
+                                    expanded={expanded === 'panel2'}
+                                    onChange={handleChange('panel2')}
+                                    elevation={0}
+                                    sx={{
+                                        m: 0,
+                                        p: 0,
+                                        color: 'primary.main',
+                                        ...getHoverStyles('panel2'),
+                                    }}>
+                                    <AccordionSummary expandIcon={<ExpandMore />} aria-controls="panel2d-content" id="panel2d-header" sx={{ my: 0, py: 0 }}>
+                                        <Typography textAlign="center">Hire Now</Typography>
+                                    </AccordionSummary>
+                                    <AccordionDetails sx={{ my: 0, py: 0 }}>
+                                        {hireOptions.map((option) => (
+                                            <MenuItem key={option} onClick={handleChange('panel2')} sx={{ color: 'black' }}>
+                                                <Typography textAlign="center">{option}</Typography>
+                                            </MenuItem>
+                                        ))}
+                                    </AccordionDetails>
+                                </Accordion>
+                                <MenuItem>
+                                    <Button variant='contained' sx={{ mx: 1 }}>
+                                        <b>Get A Free Quote</b>
+                                    </Button>
+                                </MenuItem>
+                            </Paper>
+                        )}
+                    </Box>
                     <Box sx={{ flexGrow: 1, display: { xs: 'none', sm: 'none', md: 'none', lg: 'flex', xl: 'flex' }, justifyContent: 'center', p: 0, m: 0, alignItems: 'center' }}>
                         <Box sx={{ justifyContent: 'center', alignItems: 'center', display: { xs: 'none', sm: 'none', md: 'none', lg: 'flex', xl: 'flex' } }}>
                             <Typography variant='h3' sx={{ flexGrow: 1, mx: 1 }}>LOGO</Typography>
